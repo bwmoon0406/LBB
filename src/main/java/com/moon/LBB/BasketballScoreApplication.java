bc005a88fbe283ff902901ae31d1a271c77e2ca9
moon <bwmoon0406@gamil.com>
2024-10-10 13:22:27 +0900

    1번째 commit Test

---
+++ src/main/java/com/moon/LBB/BasketballScoreApplication.java
@@ -0,0 +1,13 @@
+package com.moon.LBB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moon/LBB/BasketballScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.LBB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
